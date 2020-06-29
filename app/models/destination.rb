faeac7972214bc95055d547d2a5a4f2c4e944452
dr-dolce14 <dolcemascop1@gmail.com>
2020-06-29 17:32:54 -0400

    a couple more things lol

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,19 @@
 class Destination < ApplicationRecord
     has_many :posts
     has_many :bloggers, through: :posts 
+
+    def most_liked
+        self.posts.max_by do |post|
+             post.likes
+         end
+     end
+
+     def average_age
+        ages = self.bloggers.uniq.map do |blogger|
+            blogger.age
+     end
+     ages.sum(0.0) / ages.size
+    end
+
+
 end
